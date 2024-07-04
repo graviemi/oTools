3f8465b433a7d53aea536661a18610a62c73700f
Michel Gravier <michel.gravier@univ-grenoble-alpes.fr>
2024-07-04 11:51:43 +0200

    ajout iterator text

--- data/text.class.php
+++ data/text.class.php
@@ -2,40 +2,58 @@
 
 namespace oTools\data;
 
-class text
+use iterator;
+
+class text implements iterator
 {
 	protected string $contents;
 	protected int $length;
-	protected int $position;
+	protected int $position = 0;
+	protected int $index = 0;
+	protected string $line = '';
+	protected int $increment = 0;
 
 	public function __construct(string $contents)
 	{
 		$this->contents = $contents;
 		$this->length = strlen($contents);
-		$this->position = 0;
+	}
+
+	public function current()
+	{
+		return $this->line;
+	}
+
+	public function key()
+	{
+		return $this->index;
+	}
+
+	public function next()
+	{
+		$this->position += $this->increment;
+		if (preg_match('/\G([^\r\n]*)(\r|\n|\r\n)?/',$this->contents,$matches,0,$this->position))
+		{
+			$this->line = $matches[1];
+			$this->increment = strlen($matches[0]);
+			$this->index++;
+		}
 	}
 
 	public function rewind()
 	{
 		$this->position = 0;
-	}
-
-	public function isEnd() : bool
-	{
-		return $this->position >= $this->length;
-	}
-
-	public function nextLine() : ?string
-	{
-		if ($this->isEnd())
-			return null;
+		$this->index = 0;
 		if (preg_match('/\G([^\r\n]*)(\r|\n|\r\n)?/',$this->contents,$matches,0,$this->position))
 		{
-			$this->position += strlen($matches[0]);
-			return $matches[1];
+			$this->line = $matches[1];
+			$this->increment = strlen($matches[0]);
 		}
-		throw new exception('line match failed at %d/%d',$this->position,$this->length);
-		
+	}
+
+	public function valid()
+	{
+		return $this->position < $this->length;
 	}
 
 	public function __toString() : string
